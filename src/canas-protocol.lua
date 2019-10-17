2d6731c25e0056045d1cf79252bb7f043f1abf45
andreas <andreas.luethi@gmx.net>
2019-10-17 17:32:29 +0200

    Typo.

--- src/canas-protocol.lua
+++ src/canas-protocol.lua
@@ -16,7 +16,7 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 --
--- Run with: wireshark -X X lua_script:canas-protocol.lua
+-- Run with: wireshark -X lua_script:canas-protocol.lua
 
 RUN_TESTS = os.getenv("TEST")
 
