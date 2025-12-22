636a62d7d37fc955f7cdac4814a3d2b142a162b9
Andreas Lüthi <andreas.luethi@gmx.net>
2025-12-22 16:30:56 +0100

    Convert code to Lua 5.4.

--- src/canas-protocol.lua
+++ src/canas-protocol.lua
@@ -29,14 +29,14 @@ set_plugin_info(plugin_info)
 
 local utils = require("utils")
 
-canas_proto = Proto("canas", "CANaerospace Protocol")
+local canas_proto = Proto("canas", "CANaerospace Protocol")
 
 -- Proto header fields
 local header_fields = {
-    canid = ProtoField.uint24("canas.canid", "Can Id", base.DEC, defaultIdentifierTable),
-    nodeid = ProtoField.uint8("canas.nodeid", "Node Id", base.DEC, defaultNodeIdTable),
-    datatype = ProtoField.uint8("canas.datatype", "Data Type", base.DEC, dataTypeTable),
-    servicecode = ProtoField.uint8("canas.servicecode", "Service Code", base.DEC, serviceCodeTable)
+    canid = ProtoField.uint24("canas.canid", "Can Id", base.DEC, utils.defaultIdentifierTable),
+    nodeid = ProtoField.uint8("canas.nodeid", "Node Id", base.DEC, utils.defaultNodeIdTable),
+    datatype = ProtoField.uint8("canas.datatype", "Data Type", base.DEC, utils.dataTypeTable),
+    servicecode = ProtoField.uint8("canas.servicecode", "Service Code", base.DEC, utils.serviceCodeTable)
 }
 canas_proto.fields = header_fields
 
@@ -46,27 +46,27 @@ function canas_proto.dissector(buffer, pinfo, tree)
     local subtree = tree:add(canas_proto, buffer(), "CANaerospace Protocol Data")
 
     -- CAN part
-    subtree = subtree:add(buffer(0, 8), "CAN")
+    local can_subtree = subtree:add(buffer(0, 8), "CAN")
     local canId = buffer(0, 3):le_int()
-    subtree:add(header_fields.canid, buffer(0, 3), canId)
-    subtree:add(buffer(3, 1), "flags, xtd: " .. buffer(3, 1):bitfield(0, 1) .. " rtr: " .. buffer(3, 1):bitfield(1, 1) .. " err: " .. buffer(3, 1):bitfield(2, 1))
-    subtree:add(buffer(4, 1), "len: " .. buffer(4, 1))
-    subtree:add(buffer(5, 3), "reserved: " .. buffer(3, 3))
+    can_subtree:add(header_fields.canid, buffer(0, 3), canId)
+    can_subtree:add(buffer(3, 1), "flags, xtd: " .. buffer(3, 1):bitfield(0, 1) .. " rtr: " .. buffer(3, 1):bitfield(1, 1) .. " err: " .. buffer(3, 1):bitfield(2, 1))
+    can_subtree:add(buffer(4, 1), "len: " .. buffer(4, 1))
+    can_subtree:add(buffer(5, 3), "reserved: " .. buffer(3, 3))
 
     -- CANaerospace part
-    subtree = subtree:add(buffer(8, 8), "aerospace")
-    subtree:add(header_fields.nodeid, buffer(8, 1), buffer(8, 1):uint())
+    local aerospace_subtree = subtree:add(buffer(8, 8), "aerospace")
+    aerospace_subtree:add(header_fields.nodeid, buffer(8, 1), buffer(8, 1):uint())
     local dataType = buffer(9, 1):uint()
-    subtree:add(header_fields.datatype, buffer(9, 1), dataType)
-    subtree:add(header_fields.servicecode, buffer(10, 1), buffer(10, 1):uint())
-    subtree:add(buffer(11, 1), "Message Code: " .. buffer(11, 1):uint())
-    subtree:add(buffer(12, 4), "Data: " .. getValue(buffer(12, 4), dataType, canId))
-    info = defaultIdentifierTable[canId]
+    aerospace_subtree:add(header_fields.datatype, buffer(9, 1), dataType)
+    aerospace_subtree:add(header_fields.servicecode, buffer(10, 1), buffer(10, 1):uint())
+    aerospace_subtree:add(buffer(11, 1), "Message Code: " .. buffer(11, 1):uint())
+    aerospace_subtree:add(buffer(12, 4), "Data: " .. utils.getValue(buffer(12, 4), dataType, canId))
+    local info = utils.defaultIdentifierTable[canId]
     if info == nil then
         info = "Identifier " .. canId
     end
     pinfo.cols.info = info
 end
 
-dissector_table = DissectorTable.get("sll.ltype")
+local dissector_table = DissectorTable.get("sll.ltype")
 dissector_table:add(12, canas_proto)
